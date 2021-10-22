33dd529c7d9cc260be597e073dd72a3e522a6157
hdugan <hilarydugan@gmail.com>
2021-10-22 11:05:20 -0500

    updated physics code

--- src/run_physics.R
+++ src/run_physics.R
@@ -3,18 +3,10 @@ rm(list = ls())
 
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
-# Package ID: knb-lter-ntl.29.8 Cataloging System:https://pasta.lternet.edu.
+# Package ID: knb-lter-ntl.29.29 Cataloging System:https://pasta.edirepository.org.
 # Data set title: North Temperate Lakes LTER: Physical Limnology of Primary Study Lakes 1981 - current.
-# Data set creator:    - Center for Limnology 
-# Data set creator:    - NTL LTER 
-# Metadata Provider:    - North Temperate Lakes LTER 
-# Contact:    - Information Manager LTER Network Office  - tech-support@lternet.edu
-# Contact:    - NTL LTER Information Manager University of Wisconsin  - infomgr@lter.limnology.wisc.edu
-# Contact:    - NTL LTER Lead PI Center for Limnology  - leadpi@lter.limnology.wisc.edu
-# Metadata Link: https://portal.lternet.edu/nis/metadataviewer?packageid=knb-lter-ntl.29.8
-# Stylesheet v2.11 for metadata conversion into program: John H. Porter, Univ. Virginia, jporter@virginia.edu 
 
-inUrl3 <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/29/28/03e232a1b362900e0f059859abe8eb97"
+inUrl3 <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/29/29/03e232a1b362900e0f059859abe8eb97"
 infile3 <- tempfile()
 download.file(inUrl3, infile3, method = "curl")
 dt1 <- read_csv(infile3, skip = 1, quote = "\"", guess_max = 1e+05, 
@@ -22,125 +14,8 @@ dt1 <- read_csv(infile3, skip = 1, quote = "\"", guess_max = 1e+05,
                                    "depth", "rep", "sta", "event", "wtemp", "o2", "o2sat", 
                                    "deck", "light", "frlight", "flagdepth", "flagwtemp", 
                                    "flago2", "flago2sat", "flagdeck", "flaglight", "flagfrlight"))
-
-# 
-# inUrl1  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/29/8/1932bb71889c8e25cb216c8dc0db33d5" 
-# infile1 <- tempfile()
-# try(download.file(inUrl1,infile1,method="curl"))
-# if (is.na(file.size(infile1))) download.file(inUrl1,infile1,method="auto")
-# 
-# 
-# dt1 <-read.csv(infile1,header=F 
-#                ,skip=1
-#                ,sep=","  
-#                ,quot='"' 
-#                , col.names=c(
-#                  "lakeid",     
-#                  "year4",     
-#                  "daynum",     
-#                  "sampledate",     
-#                  "depth",     
-#                  "rep",     
-#                  "sta",     
-#                  "event",     
-#                  "wtemp",     
-#                  "o2",     
-#                  "o2sat",     
-#                  "deck",     
-#                  "light",     
-#                  "frlight",     
-#                  "flagdepth",     
-#                  "flagwtemp",     
-#                  "flago2",     
-#                  "flago2sat",     
-#                  "flagdeck",     
-#                  "flaglight",     
-#                  "flagfrlight"    ), check.names=TRUE)
-# 
-# unlink(infile1)
-# 
-# # Fix any interval or ratio columns mistakenly read in as nominal and nominal columns read as numeric or dates read as strings
-# 
-# if (class(dt1$lakeid)!="factor") dt1$lakeid<- as.factor(dt1$lakeid)
-# if (class(dt1$year4)=="factor") dt1$year4 <-as.numeric(levels(dt1$year4))[as.integer(dt1$year4) ]               
-# if (class(dt1$year4)=="character") dt1$year4 <-as.numeric(dt1$year4)
-# if (class(dt1$daynum)=="factor") dt1$daynum <-as.numeric(levels(dt1$daynum))[as.integer(dt1$daynum) ]               
-# if (class(dt1$daynum)=="character") dt1$daynum <-as.numeric(dt1$daynum)                                   
-# # attempting to convert dt1$sampledate dateTime string to R date structure (date or POSIXct)                                
-# tmpDateFormat<-"%Y-%m-%d"
-# tmp1sampledate<-as.Date(dt1$sampledate,format=tmpDateFormat)
-# # Keep the new dates only if they all converted correctly
-# if(length(tmp1sampledate) == length(tmp1sampledate[!is.na(tmp1sampledate)])){dt1$sampledate <- tmp1sampledate } else {print("Date conversion failed for dt1$sampledate. Please inspect the data and do the date conversion yourself.")}                                                                    
-# rm(tmpDateFormat,tmp1sampledate) 
-# if (class(dt1$depth)=="factor") dt1$depth <-as.numeric(levels(dt1$depth))[as.integer(dt1$depth) ]               
-# if (class(dt1$depth)=="character") dt1$depth <-as.numeric(dt1$depth)
-# if (class(dt1$rep)!="factor") dt1$rep<- as.factor(dt1$rep)
-# if (class(dt1$sta)!="factor") dt1$sta<- as.factor(dt1$sta)
-# if (class(dt1$event)!="factor") dt1$event<- as.factor(dt1$event)
-# if (class(dt1$wtemp)=="factor") dt1$wtemp <-as.numeric(levels(dt1$wtemp))[as.integer(dt1$wtemp) ]               
-# if (class(dt1$wtemp)=="character") dt1$wtemp <-as.numeric(dt1$wtemp)
-# if (class(dt1$o2)=="factor") dt1$o2 <-as.numeric(levels(dt1$o2))[as.integer(dt1$o2) ]               
-# if (class(dt1$o2)=="character") dt1$o2 <-as.numeric(dt1$o2)
-# if (class(dt1$o2sat)=="factor") dt1$o2sat <-as.numeric(levels(dt1$o2sat))[as.integer(dt1$o2sat) ]               
-# if (class(dt1$o2sat)=="character") dt1$o2sat <-as.numeric(dt1$o2sat)
-# if (class(dt1$deck)=="factor") dt1$deck <-as.numeric(levels(dt1$deck))[as.integer(dt1$deck) ]               
-# if (class(dt1$deck)=="character") dt1$deck <-as.numeric(dt1$deck)
-# if (class(dt1$light)=="factor") dt1$light <-as.numeric(levels(dt1$light))[as.integer(dt1$light) ]               
-# if (class(dt1$light)=="character") dt1$light <-as.numeric(dt1$light)
-# if (class(dt1$frlight)!="factor") dt1$frlight<- as.factor(dt1$frlight)
-# if (class(dt1$flagdepth)!="factor") dt1$flagdepth<- as.factor(dt1$flagdepth)
-# if (class(dt1$flagwtemp)!="factor") dt1$flagwtemp<- as.factor(dt1$flagwtemp)
-# if (class(dt1$flago2)!="factor") dt1$flago2<- as.factor(dt1$flago2)
-# if (class(dt1$flago2sat)!="factor") dt1$flago2sat<- as.factor(dt1$flago2sat)
-# if (class(dt1$flagdeck)!="factor") dt1$flagdeck<- as.factor(dt1$flagdeck)
-# if (class(dt1$flaglight)!="factor") dt1$flaglight<- as.factor(dt1$flaglight)
-# if (class(dt1$flagfrlight)!="factor") dt1$flagfrlight<- as.factor(dt1$flagfrlight)
-# 
-# # Convert Missing Values to NA for non-dates
-# 
-# 
-# 
-# # Here is the structure of the input data frame:
-# str(dt1)                            
-# attach(dt1)                            
-# # The analyses below are basic descriptions of the variables. After testing, they should be replaced.                 
-# 
-# summary(lakeid)
-# summary(year4)
-# summary(daynum)
-# summary(sampledate)
-# summary(depth)
-# summary(rep)
-# summary(sta)
-# summary(event)
-# summary(wtemp)
-# summary(o2)
-# summary(o2sat)
-# summary(deck)
-# summary(light)
-# summary(frlight)
-# summary(flagdepth)
-# summary(flagwtemp)
-# summary(flago2)
-# summary(flago2sat)
-# summary(flagdeck)
-# summary(flaglight)
-# summary(flagfrlight) 
-# # Get more details on character variables
-# 
-# summary(as.factor(dt1$lakeid)) 
-# summary(as.factor(dt1$rep)) 
-# summary(as.factor(dt1$sta)) 
-# summary(as.factor(dt1$event)) 
-# summary(as.factor(dt1$frlight)) 
-# summary(as.factor(dt1$flagdepth)) 
-# summary(as.factor(dt1$flagwtemp)) 
-# summary(as.factor(dt1$flago2)) 
-# summary(as.factor(dt1$flago2sat)) 
-# summary(as.factor(dt1$flagdeck)) 
-# summary(as.factor(dt1$flaglight)) 
-# summary(as.factor(dt1$flagfrlight))
-# detach(dt1)               
+dt1
+   
 
 # packages
 # install.packages('devtools')
@@ -186,11 +61,12 @@ en.df <- data.frame('sampledate' = NULL, 'energy' = NULL, 'n2' = NULL, 'id' = NU
 therm.df <- data.frame('sampledate' = NULL, 'thermdepth_m' = NULL, 'id' = NULL)
 
 for (name in ntl.id){
+  print(name)
   
   data <- dt1 %>%
     filter(lakeid == name) %>%
     group_by(sampledate) %>%
-    filter((flagwtemp) == "") %>%
+    filter(is.na(flagwtemp)) %>%
     filter(!is.na(wtemp)) %>%
     filter(sum(!is.na(wtemp))>1) %>%
     fill(wtemp, .direction = 'up') %>%
@@ -204,7 +80,7 @@ for (name in ntl.id){
   data.o2 <- dt1 %>%
     filter(lakeid == name) %>%
     group_by(sampledate) %>%
-    filter((flagwtemp) == "") %>%
+    filter(is.na(flagwtemp)) %>%
     filter(!is.na(o2)) %>%
     filter(sum(!is.na(o2))>1) %>%
     fill(o2, .direction = 'up') %>%
