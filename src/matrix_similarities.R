b94b494edac06157cc0cf416a6f93d73b4497c72
robertladwig <ladwigjena@gmail.com>
2021-10-22 15:55:11 -0500

    huh

--- src/matrix_similarities.R
+++ src/matrix_similarities.R
@@ -13,7 +13,8 @@ library(ggridges)
 library(pracma)
 library(scales)
 library(Matrix)
-
+library(RColorBrewer)
+library(corrplot)
 
 df <- read_csv('../Data/phenology_data.csv') %>%
   filter(variable != 'clearwater')
@@ -40,7 +41,7 @@ for (d in lake.id){
         x[match(n,(unique(df$variable))), match(m,(unique(data$variable)))] = data$value[match(n,unique(data$variable))] - data$value[match(m,unique(data$variable))]
       }
     }
-    
+    x <- abs(x)
     error <- try(eigen(x), silent = T)
     if (class(error) == "try-error"){
       sim.df <- rbind(sim.df, data.frame('year' = i, 
@@ -74,8 +75,60 @@ ggplot(sim.df %>% arrange(year)) +
   theme_bw()
 
 ggplot(sim.df %>% arrange(year), aes(year, (as.numeric(eigenvalue)))) +
-  geom_point(aes(year, (as.numeric(eigenvalue)), col =id)) +
-  geom_line(aes(year, (as.numeric(eigenvalue)), col = id)) +
-  geom_smooth(method = lm, formula = y ~ splines::bs(x, 3), se = FALSE)+
+  geom_point(aes(year, scale(as.numeric(eigenvalue)), col =id, group = id)) +
+  geom_line(aes(year, scale(as.numeric(eigenvalue)), col = id, group = id)) +
+  # geom_smooth(method = lm, formula = y ~ splines::bs(x, 3), se = FALSE)+
+  # facet_wrap(~ id, ncol = 1)+
+  scale_color_brewer(palette = "Paired") +
+  theme_bw()
+
+d.sim.df = sim.df %>% 
+  group_by(id, year) %>%
+  mutate(diff = c(0,diff((eigenvalue))))
+
+ggplot(d.sim.df %>% arrange(year)) +
+  geom_point(aes(year, as.numeric((diff)), group = id, col =id)) +
+  geom_line(aes(year, as.numeric((diff)), group = id, col =id)) +
   # facet_wrap(~ id, ncol = 1)+
   theme_bw()
+
+ggplot(d.sim.df %>% arrange(year)) +
+  geom_point(aes(year, (as.numeric(diff)), group = id, col =id)) +
+  geom_line(aes(year, (as.numeric(diff)), group = id, col =id)) +
+  facet_wrap(~ id, ncol = 1)+
+  theme_bw()
+
+
+lake.id = c('AL', 'BM','CR', 'SP','TR','CB', 'TB', 'FI', 'ME', 'MO', 'WI')
+corr.m <- matrix(NA, nrow = length(lake.id), ncol = length(lake.id))
+for (m in lake.id){
+  for (n in lake.id){
+    dit = sim.df %>% filter(id == m) %>% arrange(year)
+    dat =  sim.df %>% filter(id == n) %>% arrange(year)
+    error <- try(cor(dit$eigenvalue, dat$eigenvalue,  method = "pearson", use = "complete.obs"), silent = T)
+    if (class(error) == "try-error"){
+      pear = NaN
+    }else{
+      pear = cor(dit$eigenvalue, dat$eigenvalue,  method = "pearson", use = "complete.obs")
+    }
+    g <- ggplot() +
+      geom_point(data = sim.df %>% filter(id == m) %>% arrange(year),aes(year, (as.numeric(eigenvalue)), col=m, group = id)) +
+      geom_line(data = sim.df %>% filter(id == m) %>% arrange(year), aes(year, (as.numeric(eigenvalue)), col = m, group = id)) +
+      # geom_smooth(method = lm, formula = y ~ splines::bs(x, 3), se = FALSE)+
+      # facet_wrap(~ id, ncol = 1)+
+      geom_point(data = sim.df %>% filter(id == n) %>% arrange(year),aes(year, (as.numeric(eigenvalue)), col =n, group = id)) +
+      geom_line(data = sim.df %>% filter(id == n) %>% arrange(year), aes(year, (as.numeric(eigenvalue)), col = n, group = id)) +
+      ylab('eigenvalue') + xlab('')+
+      scale_color_brewer(palette = "Set2") +
+      theme_bw() +
+      ggtitle(paste0('Pearson ', pear))
+    corr.m[match(m,lake.id),match(n,lake.id)] = pear
+    ggsave(file = paste0('../Figures/eigenvalues/',m,'_',n,'.png'), g, dpi = 500, width =9, height = 3)
+  }
+}
+
+str(corr.m)
+
+colnames(corr.m) <- lake.id
+rownames(corr.m) <- lake.id
+corrplot(corr.m, method="circle")
