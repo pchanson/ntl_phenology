2f7b3b464def2486a1b2e5a46f6ac0503d413d12
hdugan <hilarydugan@gmail.com>
2022-04-07 12:42:44 -0500

    fixed a whole bunch of doc problems

--- src/nutrients.R
+++ src/nutrients.R
@@ -1,67 +1,86 @@
-#devtools::install_github('hdugan/NTLlakeloads')
-library(NTLlakeloads)
-library("tidyr")
-library(dplyr)
-library(ggplot2)
 library(lubridate)
 library(tidyverse)
-library(data.table)
+library(scales)
 
-nuts<-read_csv('C:/Program Files/Git/tmp/ntl_phenology/Data/ntl1_v9_1.csv')
-nuts$sampledate<-mdy(nuts$sampledate)
+# filtering function - turns outliers into NAs to be removed
+filter_lims <- function(x){
+  l <- boxplot.stats(x)$stats[1]
+  u <- boxplot.stats(x)$stats[5]
+  for (i in 1:length(x)){
+    x[i] <- ifelse(x[i]>l & x[i]<u, x[i], NA)
+  }
+  return(x)
+}
 
-thermo<-read_csv('C:/Program Files/Git/tmp/ntl_phenology/Data/thermocline.csv')
-names<-c("sampledate","thermdepth_m","lakeid")
-setnames(thermo,names)
+# removed flagged data
+lternuts.flagged = read_csv('Data/ntl1_v9_1.csv') %>%
+  mutate(across(everything(), ~replace(., .<0 , NA))) %>%
+  rename_all( ~ str_replace(., "_sloh", '.sloh')) %>%
+  rename_all( ~ str_replace(., "_n", '.n')) %>%
+  rename_at(vars(ph:drsif.sloh), ~ str_c("value_",.)) %>%
+  rename_at(vars(flagdepth:flagdrsif.sloh), ~ str_c("error_",.)) %>%
+  rename_all(~str_replace_all(.,"flag","")) %>%
+  pivot_longer(-(lakeid:event), names_to = c('.value','item'), names_sep = '_') %>%
+  filter(!is.na(value) & value>= 0) %>%
+  filter(!str_detect(error,'A|K|L|H') | is.na(error)) %>%
+  select(-error) %>% 
+  mutate(value = case_when(str_detect(item, ".sloh") ~ value*1000, #change sloh from mg to µg
+                           TRUE ~ value)) %>% 
+  mutate(item = case_when(str_detect(item, ".sloh") ~  str_remove(item, ".sloh"),
+                          TRUE ~ item))
 
-merged_data<-merge(nuts,thermo,by=c("lakeid","sampledate")) 
+# Load thermocline depth
+thermo <- read_csv('Data/thermocline.csv') |> 
+  rename(lakeid = id)
 
-epi<-merged_data[merged_data$depth<1,]
-
-require(plyr)
-
-epi_mean<- epi %>% 
-  group_by(lakeid,year4) %>%
-  dplyr::summarize_if(is.numeric,mean,na.rm=T)
-
-
-ggplot(data=epi,aes(x=daynum, y=totpf,col=as.factor(year4)))+
-  geom_point()+
-  geom_line()+
-  facet_wrap(~lakeid,scales="free")
-
-ggplot(data=epi,aes(x=daynum, y=drp_sloh,col=as.factor(year4)))+
-  geom_point()+
-  geom_line()+
-  facet_wrap(~lakeid,scales="free")
-
-ggplot(data=epi,aes(x=daynum, y=nh4,col=as.factor(year4)))+
-  geom_point()+
-  geom_line()+
-  facet_wrap(~lakeid,scales="free")
-
-ggplot(data=epi,aes(x=daynum, y=nh4_sloh,col=as.factor(year4)))+
-  geom_point()+
-  geom_line()+
-  facet_wrap(~lakeid,scales="free")
+# Load stratification dates
+strat = read_csv('Data/final_metric_data/physics.csv') |> 
+  filter(metric == 'straton') |> 
+  select(lakeid, year4 =year , stratday = daynum)
 
 
-ggplot(data=epi,aes(x=daynum, y=doc,col=as.factor(year4)))+
-  geom_point()+
-  geom_line()+
-  ylim(0,30)+
-  facet_wrap(~lakeid,scales="free")
+# restrict to epilimnion and stratification period
+nuts = lternuts.flagged |> left_join(thermo, by = c("lakeid", "sampledate")) |> 
+  left_join(strat, by = c("lakeid", "year4")) |> 
+  mutate(month = month(sampledate), year = year(sampledate), yday = yday(sampledate)) |> 
+  filter(depth <= thermdepth_m) |> #filter to epilimnion
+  filter(daynum >= stratday) |> #filter to after stratification
+  filter(year > 1981) 
 
-epi_less<-epi[epi$doc<30,]
+# What plot looks like with outliers
+ggplot(nuts |> filter(item == 'doc')) +
+  geom_path(aes(x = as.Date(yday, origin = as.Date('2019-01-01')), y = value, group = year)) +
+  geom_point(aes(x = as.Date(yday, origin = as.Date('2019-01-01')), y = value, group = year)) +
+  scale_x_date(labels = date_format("%b")) +
+  facet_wrap(~lakeid, scales = 'free_y') +
+  theme_bw(base_size = 9) +
+  theme(axis.title.x = element_blank())
 
-doc_mean<- epi_less %>% 
-  group_by(lakeid,year4) %>%
-  dplyr::summarize_if(is.numeric,mean,na.rm=T)
+# Exclude outliers based on box plot statistics 
+nuts2 = nuts |> 
+  group_by(lakeid) |> 
+  filter(item == 'doc') |> 
+  mutate(value = filter_lims(value)) |> 
+  group_by(lakeid, sampledate, year, yday) |> 
+  summarise(value = mean(value, na.rm = T))
 
-doc<-doc_mean[,c("lakeid","year4","daynum")]
+ggplot(nuts2) +
+  geom_path(aes(x = as.Date(yday, origin = as.Date('2019-01-01')), y = value, group = year)) +
+  geom_point(aes(x = as.Date(yday, origin = as.Date('2019-01-01')), y = value, group = year)) +
+  scale_x_date(labels = date_format("%b")) +
+  facet_wrap(~lakeid, scales = 'free_y') +
+  theme_bw(base_size = 9) +
+  theme(axis.title.x = element_blank())
 
-ggplot(data=doc,aes(x=daynum))+
-  geom_density()+
-  facet_wrap(~lakeid,scales="free")
+# find day of max DOC
+doc = nuts2 |> group_by(lakeid, year) |> 
+  filter(value == max(value, na.rm = T)) |> 
+  mutate(daynum = yday(sampledate)) |> 
+  select(lakeid, year4 = year, daynum = yday)
 
-write.csv(doc,"../Data/doc.csv")
+# Plot density distributions
+ggplot(doc) +
+  geom_density(aes(x = daynum)) +
+  facet_wrap(~lakeid)
+
+write.csv(doc,"Data/doc.csv")
---
+++ src/nutrients_old.R
@@ -0,0 +1,67 @@
+#devtools::install_github('hdugan/NTLlakeloads')
+library(NTLlakeloads)
+library("tidyr")
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(tidyverse)
+library(data.table)
+
+nuts<-read_csv('C:/Program Files/Git/tmp/ntl_phenology/Data/ntl1_v9_1.csv')
+nuts$sampledate<-mdy(nuts$sampledate)
+
+thermo<-read_csv('C:/Program Files/Git/tmp/ntl_phenology/Data/thermocline.csv')
+names<-c("sampledate","thermdepth_m","lakeid")
+setnames(thermo,names)
+
+merged_data<-merge(nuts,thermo,by=c("lakeid","sampledate")) 
+
+epi<-merged_data[merged_data$depth<1,]
+
+require(plyr)
+
+epi_mean<- epi %>% 
+  group_by(lakeid,year4) %>%
+  dplyr::summarize_if(is.numeric,mean,na.rm=T)
+
+
+ggplot(data=epi,aes(x=daynum, y=totpf,col=as.factor(year4)))+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~lakeid,scales="free")
+
+ggplot(data=epi,aes(x=daynum, y=drp_sloh,col=as.factor(year4)))+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~lakeid,scales="free")
+
+ggplot(data=epi,aes(x=daynum, y=nh4,col=as.factor(year4)))+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~lakeid,scales="free")
+
+ggplot(data=epi,aes(x=daynum, y=nh4_sloh,col=as.factor(year4)))+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~lakeid,scales="free")
+
+
+ggplot(data=epi,aes(x=daynum, y=doc,col=as.factor(year4)))+
+  geom_point()+
+  geom_line()+
+  ylim(0,30)+
+  facet_wrap(~lakeid,scales="free")
+
+epi_less<-epi[epi$doc<30,]
+
+doc_mean<- epi_less %>% 
+  group_by(lakeid,year4) %>%
+  dplyr::summarize_if(is.numeric,mean,na.rm=T)
+
+doc<-doc_mean[,c("lakeid","year4","daynum")]
+
+ggplot(data=doc,aes(x=daynum))+
+  geom_density()+
+  facet_wrap(~lakeid,scales="free")
+
+write.csv(doc,"../Data/doc.csv")
