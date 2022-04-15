0ec71c9b3b070bc50f6dcad5b239694260ca6d39
Robert Ladwig <ladwigjena@gmail.com>
2022-04-15 16:23:43 -0500

    Add files via upload

--- src/Functions/som.R
+++ src/Functions/som.R
@@ -20,26 +20,44 @@ n <- 60
 qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
 col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 
-df <- read_csv('../Data/phenology_data.csv')
+df <- read_csv('../Data/final_combined_dates_filled_v1.csv')
 
 str(df)
 
 library(kohonen)
 
-df.s <- df %>%
-  filter(variable%in%c('straton', 'stratoff', 'energy', 'stability','anoxia','daphnia','iceon',
-                      'iceoff', 'chla'))
+# df.s <- df %>%
+#   filter(variable%in%c('straton', 'stratoff', 'energy', 'stability','anoxia_summer','daphnia','iceon',
+#                       'iceoff', 'chla'))
+# 
+# df.wide <- pivot_wider(df.s, names_from = variable,
+#                        values_from = value)
+# 
+# 
+# idx <- df.wide[complete.cases(df.wide),]
+# 
+# df.matrix <- as.matrix(idx[c('straton', 'stratoff', 'energy', 'stability','anoxia_summer','daphnia','iceon',
+#                                  'iceoff', 'chla')])
 
-df.wide <- pivot_wider(df.s, names_from = variable,
-                       values_from = value)
 
+dates = read_csv("../Data/final_combined_dates_filled_v1.csv")# %>% 
+ # filter(metric %in% c('straton', 'stratoff', 'energy', 'stability', 'anoxia',
+ #                        'iceon', 'iceoff', 'daphnia', 'chla', 'doc')) 
 
-idx <- df.wide[complete.cases(df.wide),]
+dates_wide = dates %>% 
+  pivot_wider(id_cols = c("lakeid", "year"), names_from = "metric", values_from = "daynum_fill")
 
-df.matrix <- as.matrix(idx[c('straton', 'stratoff', 'energy', 'stability','anoxia','daphnia','iceon',
-                                 'iceoff', 'chla')])
+dates_wide_complete = na.omit(dates_wide)
 
-names <- paste0(idx$id)#,'_',df.wide$year)
+unique(dates_wide_complete$lakeid)
+
+df.matrix = as.matrix(dates_wide_complete %>%
+  select(c('chlor_fall','chlor_spring','secchi_openwater','daphnia_biomass',
+           'total_zoop_biomass','straton', 'stratoff', 'energy', 'stability', 'anoxia_summer',
+          'iceon', 'iceoff', 'doc')))
+
+# names <- paste0(idx$id)#,'_',df.wide$year)
+names <- paste0(dates_wide_complete$lakeid)
 n.colors <- c(rep(col_vector[1],sum(!is.na(match(names, 'AL')))),
               rep(col_vector[2],sum(!is.na(match(names, 'BM')))),
               rep(col_vector[3],sum(!is.na(match(names, 'CB')))),
@@ -71,7 +89,7 @@ plot(map, type='mapping',col = n.colors,
 
 data_train_matrix <- as.matrix(scale(df.matrix))
 
-som_grid <- somgrid(xdim = 5, ydim=5, topo="hexagonal")
+som_grid <- somgrid(xdim = 9, ydim=9, topo="hexagonal")
 
 som_model <- som(data_train_matrix, 
                  grid=som_grid, 
@@ -86,21 +104,30 @@ plot(som_model, type="count")
 
 plot(som_model, type="codes")
 
-layout(matrix(1:9,ncol=3,nrow=3,byrow=T))
+plot(som_model, type="mapping",col = n.colors,
+     label = names,pchs = names)
 
-for(i in 1:9){
+layout(matrix(1:15,ncol=5,nrow=3,byrow=T))
+
+for(i in 1:13){
   plot(som_model, type = "property", property=som_model$codes[[1]][,i], main=sprintf(
-    c('straton', 'stratoff', 'energy', 'stability','anoxia','daphnia','iceon','iceoff', 'chla')[i]), palette.name=coolBlueHotRed)
+    c('chlor_fall','chlor_spring','secchi_openwater','daphnia_biomass',
+      'total_zoop_biomass','straton', 'stratoff', 'energy', 'stability', 'anoxia_summer',
+      'iceon', 'iceoff', 'doc')[i]), palette.name=coolBlueHotRed)
 }
 
-for(i in 1:9){
+for(i in 1:13){
   var <- i #define the variable to plot 
   var_unscaled <- aggregate(as.numeric(df.matrix[,var]), by=list(som_model$unit.classif), FUN=mean, simplify=TRUE)[,2] 
   plot(som_model, type = "property", property=var_unscaled, 
        main=sprintf(
-         c('straton', 'stratoff', 'energy', 'stability','anoxia','daphnia','iceon','iceoff', 'chla')[i]), 
+         c('chlor_fall','chlor_spring','secchi_openwater','daphnia_biomass',
+           'total_zoop_biomass','straton', 'stratoff', 'energy', 'stability', 'anoxia_summer',
+           'iceon', 'iceoff', 'doc')[i]), 
        palette.name=coolBlueHotRed)
 }
+
+dev.off()
 
 mydata <- som_model$codes 
 wss <- (nrow(mydata)-1)*sum(apply(mydata[[1]],2,var)) 
@@ -110,10 +137,32 @@ for (i in 2:15) {
 plot(wss)
 
 ## use hierarchical clustering to cluster the codebook vectors
-som_cluster <- cutree(hclust(dist(som_model$codes[[1]])),7)
+som_cluster <- cutree(hclust(dist(som_model$codes[[1]])),5)
+
+plot(hclust(dist(som_model$codes[[1]])))
+
 # plot these results:
 pretty_palette <- c("#1f77b4", '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', "#7f7f7f", "#bcbd22", "#17becf","#00FF00", "#9F81F7"
                     ,"#FFFF00", "#F6CECE","#610B21")
 plot(som_model, type="mapping", bgcol = pretty_palette[som_cluster], main = "Clusters",
-     label = names,pchs = names) 
+       label = names,pchs = names, cex =0.7) 
+add.cluster.boundaries(som_model, som_cluster)
+
+pch.names = as.numeric(as.factor(names))
+
+plot(som_model, type="mapping", bgcol = pretty_palette[som_cluster], main = "Clusters",
+     pchs = pch.names) 
+add.cluster.boundaries(som_model, som_cluster)
+
+typ.names = names
+typ.names[typ.names%in%c("ME", "MO", "FI")] = 'S'
+typ.names[typ.names %in% c('AL','TR')] ='Ndr'
+typ.names[typ.names%in%c('BM','CR','SP')]='Ngw'
+typ.names[typ.names%in%c('CB','TB')]='Nb'
+
+plot(som_model, type="mapping", bgcol = pretty_palette[som_cluster], main = "Clusters",
+     pchs = typ.names, labels = typ.names) 
+add.cluster.boundaries(som_model, som_cluster)
+
+plot(som_model, type="codes", bgcol = pretty_palette[som_cluster])
 add.cluster.boundaries(som_model, som_cluster)
