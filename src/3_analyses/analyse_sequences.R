7f437e222e10aba7049e3e477c51b07bdaeb7018
robertladwig <ladwigjena@gmail.com>
2023-01-11 10:10:35 -0600

    seq plot analysis with constant transition costs

--- src/3_analyses/analyse_sequences.R
+++ src/3_analyses/analyse_sequences.R
@@ -43,8 +43,9 @@ library(seqHMM)
 ###############################
 diss <- c()
 sequencePlots = list()
+costs <- list()
 
-
+# df_red=df
 df_red = df %>%
   filter(metric %in% c('iceoff', 'iceon',
 'straton', 'stratoff', 'stability',
@@ -112,19 +113,25 @@ df_red = df %>%
     geom_tile(aes(fill = value))
   
   seqHtplot(seq, with.legend = "right", legend.prop=0.4)
-  scost <- seqsubm(seq, method = "TRATE")
+  # scost <- seqsubm(seq, method = "TRATE")
+  scost <- seqsubm(seq, method="CONSTANT", cval=2)
   mean_data = data_plot %>% group_by(event_id) %>% count(event) %>% filter(n==max(n))
   mean_data_seq = as.data.frame(t(as.matrix(match(mean_data$event,  levels(as.factor(df_red$metric))))))
   colnames(mean_data_seq) = colnames(data)
   mean_seq = seqdef(mean_data_seq)
   
   # https://medium.com/@surfingthroughlifeasapostdoc/sequence-analysis-time-use-data-atus-in-r-760d80b8d08b
-  om_time <- seqdist(seq, method = "OM", indel = 1, sm = scost)
-  seqdist(seq, method = 'OM', sm = seqsubm(seq, method="CONSTANT", cval=2))
-  om_time
+  om_time <- seqdist(seq, method = "OM", indel = 1, sm = scost, full.matrix = FALSE)
+  # seqdist(seq, method = 'OM', sm = seqsubm(seq, method="CONSTANT", cval=2))
+  om_time <- om_time#/max(om_time)
   round(om_time, 1)
   
-
+  medoid <- seqrep(seq, diss = om_time, criterion = 'dist', nrep = 1)
+  print(medoid, format = 'SPS')
+  seqrplot(seq, diss = om_time, border = NA)
+  
+  costs[[match(lakenames, unique(df_red$lakeid))]] = as.vector(om_time)
+  
   diss =append(diss, c(round(mean(om_time),2)))
   
   sequencePlots[[match(lakenames, unique(df_red$lakeid))]] <-
@@ -143,17 +150,23 @@ df_red = df %>%
     (sequencePlots[[8]] +   sequencePlots[[9]])  +  plot_layout(guides = 'collect')
   ggsave(filename = 'Figures/sequencePlot.png', dpi = 300, width = 7, height = 8)
   
-  sequencePlots[[1]] /
-  sequencePlots[[2]] / 
-    sequencePlots[[3]] /
-    sequencePlots[[4]] /
-    sequencePlots[[5]] /
-    sequencePlots[[6]] /
-    sequencePlots[[7]] /
-    sequencePlots[[8]] /
-    sequencePlots[[9]] /
-    sequencePlots[[10]] /
-    sequencePlots[[11]] +  plot_layout(guides = 'collect')
-  ggsave(filename = 'Figures/sequencePlot.png', dpi = 300, width = 7, height = 18)
+
+  
+  ggplot() +
+    geom_density(data = as.data.frame(costs[[2]]), aes(x = costs[[2]])) + 
+    geom_density(data = as.data.frame(costs[[7]]), aes(x = costs[[7]])) + 
+    geom_density(data = as.data.frame(costs[[4]]), aes(x = costs[[4]])) + 
+    geom_density(data = as.data.frame(costs[[5]]), aes(x = costs[[5]]))  
+  
+  ggplot() +
+    geom_density(data = as.data.frame(costs[[3]]), aes(x = costs[[3]])) +
+    geom_density(data = as.data.frame(costs[[6]]), aes(x = costs[[6]])) 
+
+  ggplot() +
+    geom_density(data = as.data.frame(costs[[1]]), aes(x = costs[[1]])) +
+    geom_density(data = as.data.frame(costs[[10]]), aes(x = costs[[10]])) 
+  ggplot() +
+    geom_density(data = as.data.frame(costs[[8]]), aes(x = costs[[8]])) +
+    geom_density(data = as.data.frame(costs[[9]]), aes(x = costs[[9]])) 
   
   
