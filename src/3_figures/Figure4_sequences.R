57af4fe51cde98fc62eb8db4fbcd1792a3e45b0f
hdugan <hilarydugan@gmail.com>
2023-01-23 13:22:50 -0600

    updated sequence plot

--- src/3_figures/Figure4_sequences.R
+++ src/3_figures/Figure4_sequences.R
@@ -11,6 +11,7 @@ library(ggpubr)
 library(ggpval)
 library(pacman)
 library(TraMineR)
+library(MetBrewer)
 # library(condSURV)
 
 data <-  read_csv('Data/analysis_ready/final_combined_dates_filled_v2.csv')
@@ -47,16 +48,18 @@ costs <- list()
 
 # df_red=df
 df_red = df %>%
-  filter(metric %in% c('iceoff', 'iceon',
-'straton', 'stratoff', 'stability',
-'anoxia_summer', 'secchi_max', 'secchi_min', 'zoopDensity'))
+  filter(metric %in% c('iceoff', 'iceon', 'straton', 'stratoff', 'stability', 'anoxia_summer', 'secchi_max')) |> 
+  mutate(metric = factor(metric, levels = c( 'iceoff', 'straton','secchi_max','stability', 'anoxia_summer', 'stratoff','iceon')))
 
-  metric_n <- length(unique(df_red$metric))
-  
-  data = c()
-  data_plot = c()
-  
-  for (lakenames in unique(df_red$lakeid)){
+#'secchi_min', 
+#'zoopDensity'))
+
+metric_n <- length(unique(df_red$metric))
+
+data = c()
+data_plot = c()
+
+for (lakenames in unique(df_red$lakeid)){
     data_HMM = df_red %>%
       filter(lakeid == lakenames) %>%
       mutate(year_norm = year - min(year),
@@ -99,7 +102,7 @@ df_red = df %>%
     
     data =as.data.frame(data_HMM[, -1])
   
-  pheno_seq <- seqdef(data, start = 1, labels = levels(as.factor(df_red$metric)))
+  pheno_seq <- seqdef(data, start = 1, labels = levels(df_red$metric))
   
 
   
@@ -109,14 +112,14 @@ df_red = df %>%
   
   trate = seqtrate(seq)
   heatTrate = reshape2::melt(trate)
-  ggplot(heatTrate, aes(Var2, Var1)) +
-    geom_tile(aes(fill = value))
-  
-  seqHtplot(seq, with.legend = "right", legend.prop=0.4)
+  # ggplot(heatTrate, aes(Var2, Var1)) +
+  #   geom_tile(aes(fill = value))
+  # 
+  # seqHtplot(seq, with.legend = "right", legend.prop=0.4)
   # scost <- seqsubm(seq, method = "TRATE")
   scost <- seqsubm(seq, method="CONSTANT", cval=2)
   mean_data = data_plot %>% group_by(event_id) %>% count(event) %>% filter(n==max(n))
-  mean_data_seq = as.data.frame(t(as.matrix(match(mean_data$event,  levels(as.factor(df_red$metric))))))
+  mean_data_seq = as.data.frame(t(as.matrix(match(mean_data$event,  levels(df_red$metric)))))
   colnames(mean_data_seq) = colnames(data)
   mean_seq = seqdef(mean_data_seq)
   
@@ -128,45 +131,58 @@ df_red = df %>%
   
   medoid <- seqrep(seq, diss = om_time, criterion = 'dist', nrep = 1)
   print(medoid, format = 'SPS')
-  seqrplot(seq, diss = om_time, border = NA)
+  # seqrplot(seq, diss = om_time, border = NA)
   
   costs[[match(lakenames, unique(df_red$lakeid))]] = as.vector(om_time)
   
   diss =append(diss, c(round(mean(om_time),2)))
   
-  sequencePlots[[match(lakenames, unique(df_red$lakeid))]] <-
+  sequencePlots[[lakenames]] <-
     ggplot(data = data_plot %>% group_by(event_id) %>% count(event),
            aes(x = event_id, y = (n*100)/max(n), fill = event)) +
+    scale_fill_manual(values = rev(met.brewer("Redon", metric_n)), name = 'metric') + 
+    scale_y_continuous(expand = c(0,0)) +
     geom_bar(stat = 'identity') + ylab('Occurrence (%)') +
     xlab('') +
+    theme_bw(base_size = 9) +
+    theme(axis.title.x = element_blank(),
+          axis.text.x = element_text(size = 6),
+          plot.title = element_text(size = 8)) +
     ggtitle(paste0(lakenames,': ',round(mean(om_time),2)))
-  }
+  
+}
 
-  
-  (sequencePlots[[2]] +   sequencePlots[[7]]) /
-    (sequencePlots[[4]] +   sequencePlots[[5]]) /
-    (sequencePlots[[3]] +   sequencePlots[[6]]) /
-    (sequencePlots[[1]] +   sequencePlots[[10]]) /
-    (sequencePlots[[8]] +   sequencePlots[[9]])  +  plot_layout(guides = 'collect')
-  ggsave(filename = 'Figures/sequencePlot.png', dpi = 300, width = 7, height = 8)
-  
 
-  
-  ggplot() +
-    geom_density(data = as.data.frame(costs[[2]]), aes(x = costs[[2]])) + 
-    geom_density(data = as.data.frame(costs[[7]]), aes(x = costs[[7]])) + 
-    geom_density(data = as.data.frame(costs[[4]]), aes(x = costs[[4]])) + 
-    geom_density(data = as.data.frame(costs[[5]]), aes(x = costs[[5]]))  
-  
-  ggplot() +
-    geom_density(data = as.data.frame(costs[[3]]), aes(x = costs[[3]])) +
-    geom_density(data = as.data.frame(costs[[6]]), aes(x = costs[[6]])) 
 
-  ggplot() +
-    geom_density(data = as.data.frame(costs[[1]]), aes(x = costs[[1]])) +
-    geom_density(data = as.data.frame(costs[[10]]), aes(x = costs[[10]])) 
-  ggplot() +
-    geom_density(data = as.data.frame(costs[[8]]), aes(x = costs[[8]])) +
-    geom_density(data = as.data.frame(costs[[9]]), aes(x = costs[[9]])) 
-  
+p.out = (sequencePlots[['BM']] +   sequencePlots[['TR']]) /
+(sequencePlots[['CR']] +   sequencePlots[['SP']]) /
+(sequencePlots[['CB']] +   sequencePlots[['TB']]) /
+  (sequencePlots[['ME']] +   sequencePlots[['MO']]) /
+(sequencePlots[['AL']] +   sequencePlots[['WI']]) +  plot_layout(guides = 'collect') &
+  theme(legend.position = 'bottom',
+        legend.margin=margin(t = 0, unit='cm'),
+        legend.key.size = unit(0.3,'cm')) & 
+  guides(fill = guide_legend(nrow = 1))
+
+ggsave(p.out, filename = 'Figures_manuscript/Figure4.png', dpi = 500, width = 6, height = 6)
+
+
+
+# ggplot() +
+#   geom_density(data = as.data.frame(costs[[2]]), aes(x = costs[[2]])) + 
+#   geom_density(data = as.data.frame(costs[[7]]), aes(x = costs[[7]])) + 
+#   geom_density(data = as.data.frame(costs[[4]]), aes(x = costs[[4]])) + 
+#   geom_density(data = as.data.frame(costs[[5]]), aes(x = costs[[5]]))  
+# 
+# ggplot() +
+#   geom_density(data = as.data.frame(costs[[3]]), aes(x = costs[[3]])) +
+#   geom_density(data = as.data.frame(costs[[6]]), aes(x = costs[[6]])) 
+# 
+# ggplot() +
+#   geom_density(data = as.data.frame(costs[[1]]), aes(x = costs[[1]])) +
+#   geom_density(data = as.data.frame(costs[[10]]), aes(x = costs[[10]])) 
+# ggplot() +
+#   geom_density(data = as.data.frame(costs[[8]]), aes(x = costs[[8]])) +
+#   geom_density(data = as.data.frame(costs[[9]]), aes(x = costs[[9]])) 
+#   
   
